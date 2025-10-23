fd990a0f045590f521694ad5f9e3b02457d638d9
chu <leetchuk21@gmail.com>
2025-10-23 23:31:33 +0800

    2025/10/23 , python print,variable,string function

---
+++ 1.py
@@ -0,0 +1,10 @@
+int_x=10
+double_x=4.99
+bool_x=True
+string_x="uuhabc"
+string_y="uuh \\ uuh" # strring with escape character
+
+print(int_x,double_x,bool_x,string_x*10)
+print(len(string_x*10))
+print(string_x[0],string_x[-1],string_x[0:2],string_x[0:],string_x[:3],string_x[:])
+print(string_y)
