96017b0a42dc18e86c3022e9a477d53c2a220ae7
chu <leetchuk21@gmail.com>
2025-10-24 13:16:14 +0800

    2025/10/24 string operation

---
+++ 3.py
@@ -0,0 +1,5 @@
+str_var="  Dynamic Programming "
+print(f"""{str_var.upper()}\n{str_var.lower()}\n{str_var.title()}\n{str_var.strip()}\n{str_var.find("Pro")}\n""")
+print("{}\n{}\n{}\n{}\n{}\n".format(str_var.upper(),str_var.lower(),str_var.title(),str_var.strip(),str_var.find("Pro")))
+print(str_var.replace("m","0"))
+print("Pro" in str_var and "D" not in str_var)
