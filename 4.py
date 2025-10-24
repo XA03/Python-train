5257867bed06a76766aa48066d92dad5531ec9de
chu <leetchuk21@gmail.com>
2025-10-24 13:41:29 +0800

    2025/10/24 variable operation

---
+++ 4.py
@@ -0,0 +1,13 @@
+print(12+1)
+print(12-1)
+print(12*2)
+print(12/5)
+print(12//5)
+print(12%5)
+print(12**2)
+
+x=68
+x=x+3
+print(x)
+x+=3
+print(x)
---
+++ 5.py
@@ -0,0 +1,7 @@
+print(round(3.14159,4))
+print(abs(-12))
+
+x=input("x=") #input type default is string.
+print(type(x),x)
+
+# int(),float(),bool(),str() functions can modify variable type.
