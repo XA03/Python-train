f9a905d1c0b9d70d58d77e193fa167375c790519
chu <leetchuk21@gmail.com>
2025-10-24 16:48:44 +0800

    2025/10/24 If condition

---
+++ 6.py
@@ -0,0 +1,13 @@
+uuh=int(input("uuh="))
+if uuh ==222:
+    print("uuh=222")
+elif uuh >50:
+    print("uuh>50 and uuh!=222")
+else :
+    print("uuh<=50 and uuh!=222")
+
+print("If condition statement done.")
+
+oneline="eeh" if uuh==222 else "wawa"
+
+print(oneline)
