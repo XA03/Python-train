f31f3a2427ef19db87c217fe3339644c0409a66d
chu <leetchuk21@gmail.com>
2025-11-02 01:02:39 +0800

    2025/11/02 Dictionary container operatios udpate.

--- dict.py
+++ dict.py
@@ -1,7 +1,19 @@
 var_dict={}
 var_dict2=dict()
 
-var_dict={'a':1}; var_dict['b']=24; print(var_dict)#var_dict={'a':1,'b':2}
-del var_dict['a']; print(var_dict)#var_dict={'b':2}
-var_dict['a']=1; print(len(var_dict),str(var_dict))
-del var_dict; print(var_dict)#it should show error messages due to printing a None dictionary.
+var_dict={'a':1}; var_dict['b']=24; print("var_dict: {}".format(var_dict))#var_dict={'a':1,'b':24}
+var_dict2=var_dict.copy(); print("var_dict2: {}".format(var_dict2))#var_dict2={'a':1,'b':24}
+del var_dict['a']; print("var_dict: {}".format(var_dict))#var_dict={'b':24}
+var_dict.clear(); print("var_dict: {}".format(var_dict))#var_dict2={'a':1,'b':24}
+#del var_dict; print(var_dict) #it should show error messages due to printing a None dictionary.
+
+x=['a','b','c']
+var_dict_fky=dict.fromkeys(x,1); print(var_dict_fky)
+x=var_dict_fky.get('a'); print(x)
+x=var_dict_fky.items(); print(x)
+var_dict_fky.pop("a"); print(var_dict_fky)
+x=var_dict_fky.popitem(); print(x)
+x=var_dict_fky.setdefault('b'); print(var_dict_fky,x)
+x=var_dict_fky.setdefault('a'); print(var_dict_fky,x)
+var_dict_fky.update({'a':1}); print(var_dict_fky)
+print(var_dict_fky,"",var_dict_fky.values())
